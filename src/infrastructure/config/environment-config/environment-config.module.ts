e07a4a95b3c6f92e533a53965d2e28f2a1a14c28
Marcelo Prates <mpratesjunior@gmail.com>
2023-03-10 20:04:09 -0300

    remove sample api

--- src/infrastructure/config/environment-config/environment-config.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EnvironmentConfigService } from './environment-config.service';
-
-@Module({
-  providers: [EnvironmentConfigService],
-})
-// eslint-disable-next-line prettier/prettier
-export class EnvironmentConfigModule { }
